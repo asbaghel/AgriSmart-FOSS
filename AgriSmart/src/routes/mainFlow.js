c966d3c149297d19184c65bf18bc5d215a847e43
asbaghel <abhisheksbaghel99@gmail.com>
2020-09-13 18:50:57 +0530

    phoneno length validation added

--- AgriSmart/src/routes/mainFlow.js
+++ AgriSmart/src/routes/mainFlow.js
@@ -2,12 +2,18 @@ import {createStackNavigator} from 'react-navigation-stack';
 import {createSwitchNavigator} from 'react-navigation';
 import FarmerHomeScreen from '../screens/FarmerHomeScreen';
 import TransporterHomeScreen from '../screens/TransporterHomeScreen';
-import DrawerNavigation from './drawer'
+import DrawerNavigation from './drawer';
+import FindTransporterScreen from '../screens/FindTransporters';
+import PaymentScreen from '../screens/PaymentScreen';
+import NavigateToFarmerScreen from '../screens/NavigateToFarmerScreen';
+import PoolingFarmerNavigationScreen from '../screens/PoolingFarmerNavigation';
 
 const mainFlow = createSwitchNavigator({
  
   farmerFlow: createStackNavigator({
     FarmerHome: FarmerHomeScreen,
+    FindTransporter: FindTransporterScreen,
+    Payment: PaymentScreen
   },{
     defaultNavigationOptions: {
       header: null
@@ -15,6 +21,9 @@ const mainFlow = createSwitchNavigator({
   }),
   transporterFlow: createStackNavigator({
     TransporterHome: TransporterHomeScreen,
+    Navigate: NavigateToFarmerScreen,
+    Pooling: PoolingFarmerNavigationScreen
+
   },{
     defaultNavigationOptions: {
       header: null
